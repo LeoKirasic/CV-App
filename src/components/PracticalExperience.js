daf9572b4bb5003d9ae6d85530e159fe5b19a504
Leo Kirasic <kirasicleo@gmail.com>
2022-01-25 15:58:13 +0100

    Refactor PracticalExperience with functional components

--- src/components/PracticalExperience.js
+++ src/components/PracticalExperience.js
@@ -1,69 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class PracticalExperience extends React.Component {
-  onFocus(event) {
+function PracticalExperience(props) {
+  function onFocus(event) {
     event.currentTarget.type = 'date';
   }
-  render() {
-    return (
-      <form className="practical-experience-form">
-        <header className="practical-experience-header section-header">
-          PRACTICAL EXPERIENCE
-        </header>
-        <label className="company-name-label">
-          <input
-            id="company-name"
-            type="text"
-            placeholder="Company Name"
-            onChange={(event) =>
-              this.props.onCompanyNameChange(event.target.value)
-            }
-          />
-        </label>{' '}
-        <br />
-        <label className="title-label">
-          <input
-            id="title"
-            type="text"
-            placeholder="Title"
-            onChange={(event) => this.props.onTitleChange(event.target.value)}
-          />
-        </label>{' '}
-        <br />
-        <label className="start-date-label">
-          <input
-            id="start-date"
-            type="text"
-            placeholder="Start Date"
-            onFocus={this.onFocus}
-            onChange={(event) =>
-              this.props.onStartDateChange(event.target.value)
-            }
-          />
-        </label>{' '}
-        <br />
-        <label className="end-date-label">
-          <input
-            id="end-date"
-            type="text"
-            placeholder="End Date"
-            onFocus={this.onFocus}
-            onChange={(event) => this.props.onEndDateChange(event.target.value)}
-          />
-        </label>{' '}
-        <br />
-        <label className="tasks-label">
-          <textarea
-            id="tasks"
-            type="text"
-            placeholder="Tasks"
-            onChange={(event) => this.props.onTasksChange(event.target.value)}
-          />
-        </label>
-      </form>
-    );
-  }
+  return (
+    <form className="practical-experience-form">
+      <header className="practical-experience-header section-header">
+        PRACTICAL EXPERIENCE
+      </header>
+      <label className="company-name-label">
+        <input
+          id="company-name"
+          type="text"
+          placeholder="Company Name"
+          onChange={(event) => props.onCompanyNameChange(event.target.value)}
+        />
+      </label>{' '}
+      <br />
+      <label className="title-label">
+        <input
+          id="title"
+          type="text"
+          placeholder="Title"
+          onChange={(event) => props.onTitleChange(event.target.value)}
+        />
+      </label>{' '}
+      <br />
+      <label className="start-date-label">
+        <input
+          id="start-date"
+          type="text"
+          placeholder="Start Date"
+          onFocus={onFocus}
+          onChange={(event) => props.onStartDateChange(event.target.value)}
+        />
+      </label>{' '}
+      <br />
+      <label className="end-date-label">
+        <input
+          id="end-date"
+          type="text"
+          placeholder="End Date"
+          onFocus={onFocus}
+          onChange={(event) => props.onEndDateChange(event.target.value)}
+        />
+      </label>{' '}
+      <br />
+      <label className="tasks-label">
+        <textarea
+          id="tasks"
+          type="text"
+          placeholder="Tasks"
+          onChange={(event) => props.onTasksChange(event.target.value)}
+        />
+      </label>
+    </form>
+  );
 }
 
 PracticalExperience.propTypes = {
