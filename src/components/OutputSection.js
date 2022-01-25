cf068387119d684304d4622c89b5f4d8ae15fae2
Leo Kirasic <kirasicleo@gmail.com>
2022-01-25 15:56:57 +0100

    Refactor OutputSection with functional components

--- src/components/OutputSection.js
+++ src/components/OutputSection.js
@@ -1,46 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class OutputSection extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div id="output-section">
-        <div className="general-information">
-          GENERAL INFORMATION
-          <div className="name">Name: {this.props.name}</div>
-          <div className="email">Email: {this.props.email}</div>
-          <div className="phone-number">
-            Phone Number: {this.props.phoneNumber}
-          </div>
+function OutputSection(props) {
+  return (
+    <div id="output-section">
+      <div className="general-information">
+        GENERAL INFORMATION
+        <div className="name">Name: {props.name}</div>
+        <div className="email">Email: {props.email}</div>
+        <div className="phone-number">Phone Number: {props.phoneNumber}</div>
+      </div>
+      <div className="educational-experience">
+        EDUCATIONAL EXPERIENCE
+        <div className="school-name">School Name: {props.schoolName}</div>
+        <div className="title-of-study">
+          Title Of Study: {props.titleOfStudy}
         </div>
-        <div className="educational-experience">
-          EDUCATIONAL EXPERIENCE
-          <div className="school-name">
-            School Name: {this.props.schoolName}
-          </div>
-          <div className="title-of-study">
-            Title Of Study: {this.props.titleOfStudy}
-          </div>
-          <div className="date-of-study">
-            From: {this.props.startDateOfStudy} To: {this.props.endDateOfStudy}
-          </div>
-        </div>
-        <div className="practical-experience">
-          PRACTICAL EXPERIENCE
-          <div className="company-name">
-            Company name: {this.props.companyName}
-          </div>
-          <div className="title">Title: {this.props.title}</div>
-          <div className="start-date">Start Date: {this.props.startDate}</div>
-          <div className="end-date">End Date: {this.props.endDate}</div>
-          <div className="tasks">Tasks: {this.props.tasks}</div>
+        <div className="date-of-study">
+          From: {props.startDateOfStudy} To: {props.endDateOfStudy}
         </div>
       </div>
-    );
-  }
+      <div className="practical-experience">
+        PRACTICAL EXPERIENCE
+        <div className="company-name">Company name: {props.companyName}</div>
+        <div className="title">Title: {props.title}</div>
+        <div className="start-date">Start Date: {props.startDate}</div>
+        <div className="end-date">End Date: {props.endDate}</div>
+        <div className="tasks">Tasks: {props.tasks}</div>
+      </div>
+    </div>
+  );
 }
 
 OutputSection.propTypes = {
