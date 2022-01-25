be23ca2b9e03954504b6b3f9db927abb943f49d4
Leo Kirasic <kirasicleo@gmail.com>
2022-01-25 15:51:06 +0100

    Refactor EducationalInfo component to use functional components

--- src/components/EducationalExperience.js
+++ src/components/EducationalExperience.js
@@ -1,66 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-class EducationalExperience extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  onFocus(event) {
+function EducationalExperience(props) {
+  function onFocus(event) {
     event.currentTarget.type = 'date';
   }
-  render() {
-    return (
-      <form className="educational-experience">
-        <div className="educational-experience-header section-header">
-          EDUCATIONAL EXPERIENCE
-        </div>
-        <label className="school-name-label">
-          <input
-            id="school-name"
-            type="text"
-            placeholder="School Name"
-            onChange={(event) =>
-              this.props.onSchoolNameChange(event.target.value)
-            }
-          />
-        </label>{' '}
-        <br />
-        <label className="title-of-study-label">
-          <input
-            id="title-of-study"
-            type="text"
-            placeholder="Title"
-            onChange={(event) =>
-              this.props.onTitleOfStudyChange(event.target.value)
-            }
-          />
-        </label>{' '}
-        <br />
-        <label className="start-date-of-study-label">
-          <input
-            id="start-date-of-study"
-            type="text"
-            onFocus={this.onFocus}
-            placeholder="Starting Date"
-            onChange={(event) =>
-              this.props.onStartDateOfStudyChange(event.target.value)
-            }
-          />
-        </label>{' '}
-        <br />
-        <label className="end-date-of-study-label">
-          <input
-            id="end-date-of-study"
-            type="text"
-            onFocus={this.onFocus}
-            placeholder="Ending Date"
-            onChange={(event) =>
-              this.props.onEndDateOfStudyChange(event.target.value)
-            }
-          />
-        </label>
-      </form>
-    );
-  }
+
+  return (
+    <form className="educational-experience">
+      <div className="educational-experience-header section-header">
+        EDUCATIONAL EXPERIENCE
+      </div>
+      <label className="school-name-label">
+        <input
+          id="school-name"
+          type="text"
+          placeholder="School Name"
+          onChange={(event) => props.onSchoolNameChange(event.target.value)}
+        />
+      </label>{' '}
+      <br />
+      <label className="title-of-study-label">
+        <input
+          id="title-of-study"
+          type="text"
+          placeholder="Title"
+          onChange={(event) => props.onTitleOfStudyChange(event.target.value)}
+        />
+      </label>{' '}
+      <br />
+      <label className="start-date-of-study-label">
+        <input
+          id="start-date-of-study"
+          type="text"
+          onFocus={onFocus}
+          placeholder="Starting Date"
+          onChange={(event) =>
+            props.onStartDateOfStudyChange(event.target.value)
+          }
+        />
+      </label>{' '}
+      <br />
+      <label className="end-date-of-study-label">
+        <input
+          id="end-date-of-study"
+          type="text"
+          onFocus={onFocus}
+          placeholder="Ending Date"
+          onChange={(event) => props.onEndDateOfStudyChange(event.target.value)}
+        />
+      </label>
+    </form>
+  );
 }
 
 EducationalExperience.propTypes = {
