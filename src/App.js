3cabb2b587a9537d9195f3b3e1445636f5fa6cf2
Leo Kirasic <kirasicleo@gmail.com>
2022-01-25 15:48:32 +0100

    Refactor App.js to use functional components

--- src/App.js
+++ src/App.js
@@ -1,74 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import InputSection from './components/InputSection';
 import OutputSection from './components/OutputSection';
 import Header from './components/Header';
 import PropTypes from 'prop-types';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [schoolName, setSchoolName] = useState('');
+  const [titleOfStudy, SetTitleOfStudy] = useState('');
+  const [startDateOfStudy, setStartDateOfStudy] = useState('');
+  const [endDateOfStudy, setEndDateOfStudy] = useState('');
+  const [companyName, setCompanyName] = useState('');
+  const [title, setTitle] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [tasks, setTasks] = useState('');
 
-    this.state = {
-      name: '',
-      email: '',
-      phoneNumber: '',
-
-      schoolName: '',
-      titleOfStudy: '',
-      startDateOfStudy: '',
-      endDateOfStudy: '',
-
-      companyName: '',
-      title: '',
-      startDate: '',
-      endDate: '',
-      tasks: '',
-    };
-  }
-
-  render() {
-    return (
-      <div id="main-container">
-        <Header />
-        <InputSection
-          onSchoolNameChange={(value) => this.setState({ schoolName: value })}
-          schoolName={this.state.schoolName}
-          onTitleOfStudyChange={(value) =>
-            this.setState({ titleOfStudy: value })
-          }
-          onStartDateOfStudyChange={(value) =>
-            this.setState({ startDateOfStudy: value })
-          }
-          onEndDateOfStudyChange={(value) =>
-            this.setState({ endDateOfStudy: value })
-          }
-          onNameChange={(value) => this.setState({ name: value })}
-          onEmailChange={(value) => this.setState({ email: value })}
-          onPhoneNumberChange={(value) => this.setState({ phoneNumber: value })}
-          onCompanyNameChange={(value) => this.setState({ companyName: value })}
-          onTitleChange={(value) => this.setState({ title: value })}
-          onStartDateChange={(value) => this.setState({ startDate: value })}
-          onEndDateChange={(value) => this.setState({ endDate: value })}
-          onTasksChange={(value) => this.setState({ tasks: value })}
-        />
-        <OutputSection
-          schoolName={this.state.schoolName}
-          titleOfStudy={this.state.titleOfStudy}
-          startDateOfStudy={this.state.startDateOfStudy}
-          endDateOfStudy={this.state.endDateOfStudy}
-          name={this.state.name}
-          email={this.state.email}
-          phoneNumber={this.state.phoneNumber}
-          companyName={this.state.companyName}
-          title={this.state.title}
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          tasks={this.state.tasks}
-        />
-      </div>
-    );
-  }
+  return (
+    <div id="main-container">
+      <Header />
+      <InputSection
+        onSchoolNameChange={(value) => setSchoolName(value)}
+        onTitleOfStudyChange={(value) => SetTitleOfStudy(value)}
+        onStartDateOfStudyChange={(value) => setStartDateOfStudy(value)}
+        onEndDateOfStudyChange={(value) => setEndDateOfStudy(value)}
+        onNameChange={(value) => setName(value)}
+        onEmailChange={(value) => setEmail(value)}
+        onPhoneNumberChange={(value) => setPhoneNumber(value)}
+        onCompanyNameChange={(value) => setCompanyName(value)}
+        onTitleChange={(value) => setTitle(value)}
+        onStartDateChange={(value) => setStartDate(value)}
+        onEndDateChange={(value) => setEndDate(value)}
+        onTasksChange={(value) => setTasks(value)}
+      />
+      <OutputSection
+        schoolName={schoolName}
+        titleOfStudy={titleOfStudy}
+        startDateOfStudy={startDateOfStudy}
+        endDateOfStudy={endDateOfStudy}
+        name={name}
+        email={email}
+        phoneNumber={phoneNumber}
+        companyName={companyName}
+        title={title}
+        startDate={startDate}
+        endDate={endDate}
+        tasks={tasks}
+      />
+    </div>
+  );
 }
 
 App.propTypes = {
