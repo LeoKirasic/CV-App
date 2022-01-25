1cd7a24be3cdbac3b265c5b26ced011c9ee07ba3
Leo Kirasic <kirasicleo@gmail.com>
2022-01-25 15:52:16 +0100

    Refactor GeneralInformation component to use functional components

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -1,48 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class GeneralInformation extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <form className="general-information-form">
-        <div className="general-information-header section-header">
-          GENERAL INFORMATION
-        </div>
-        <label className="name-label">
-          <input
-            type="text"
-            id="name"
-            placeholder="Name"
-            onChange={(event) => this.props.onNameChange(event.target.value)}
-          />
-        </label>{' '}
-        <br />
-        <label className="email-label">
-          <input
-            type="text"
-            id="email"
-            placeholder="Email"
-            onChange={(event) => this.props.onEmailChange(event.target.value)}
-          />
-        </label>{' '}
-        <br />
-        <label className="phone-number-label">
-          <input
-            type="text"
-            id="phone-number"
-            placeholder="Phone Number"
-            onChange={(event) =>
-              this.props.onPhoneNumberChange(event.target.value)
-            }
-          />
-        </label>
-      </form>
-    );
-  }
+function GeneralInformation(props) {
+  return (
+    <form className="general-information-form">
+      <div className="general-information-header section-header">
+        GENERAL INFORMATION
+      </div>
+      <label className="name-label">
+        <input
+          type="text"
+          id="name"
+          placeholder="Name"
+          onChange={(event) => props.onNameChange(event.target.value)}
+        />
+      </label>{' '}
+      <br />
+      <label className="email-label">
+        <input
+          type="text"
+          id="email"
+          placeholder="Email"
+          onChange={(event) => props.onEmailChange(event.target.value)}
+        />
+      </label>{' '}
+      <br />
+      <label className="phone-number-label">
+        <input
+          type="text"
+          id="phone-number"
+          placeholder="Phone Number"
+          onChange={(event) => props.onPhoneNumberChange(event.target.value)}
+        />
+      </label>
+    </form>
+  );
 }
 GeneralInformation.propTypes = {
   onNameChange: PropTypes.func,
